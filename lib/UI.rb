b711a2bf577fa8a031f3b574d9b39196ab38a484
marijusiMac <marijuslau@yahoo.com>
2017-12-05 02:42:21 +0200

    UI login func is done

--- lib/UI.rb
+++ lib/UI.rb
@@ -1,4 +1,5 @@
 require 'tk'
+require 'tkextlib/tile'
 require_relative 'client'
 require_relative 'admin'
 require_relative 'event'
@@ -6,105 +7,69 @@ require_relative 'field'
 require_relative 'invoice'
 require_relative 'reservation'
 
-# Windows
-root = TkRoot.new
-signin_win = TkToplevel.new
-signup_win = TkToplevel.new
-main_win = TkToplevel.new
-
-# Leave only root when starting
-signin_win.withdraw
-signup_win.withdraw
-main_win.withdraw
-
 # Data
-fields = [Field.new('Anfield', 100), Field.new('Wembley', 200)]
-clients = { 'mrjslau' => Client.new('c1510766', 'mrjslau', 'foot', 'mar@test.com') }
+@clients = { 'mrjslau'=>Client.new('c1510766', 'mrjslau', 'foot', 'mar@test.com') }
 
-# root widgets
-TkLabel.new(root, :text=>'Welcome to Fields reservations').pack
-TkButton.new(root, :text=>'Sign In',
-             :command=>proc{root.withdraw; signin_win.deiconify}).pack
-TkButton.new(root, :text=>'Sign Up',
-             :command=>proc{root.withdraw; signup_win.deiconify}).pack
+# Windows
+root = TkRoot.new { title "Home" }
+log_win = TkToplevel.new { title "Welcome" }
 
-# frames
+root.withdraw
 
+# LOGIN
+# ==============================================================================
+TkGrid.columnconfigure log_win, 0, :weight => 1
+TkGrid.rowconfigure log_win, 0, :weight => 1
+# Log Frames -------------------------------------------------------------------
+logincont = Tk::Tile::Labelframe.new(log_win) {text "Log in";
+          padding "30 20 30 20"; borderwidth "2"; relief "sunken"}
+          .grid( :column => 1, :row => 1, :sticky => 'n')
+signupcont = Tk::Tile::Labelframe.new(log_win) {text "Sign Up";
+          padding "30 20 30 20"; borderwidth "2"; relief "sunken"}
+          .grid( :column => 2, :row => 1, :sticky => 'n')
 
-# signin_win widgets
-labelInUsr = TkLabel.new(signin_win, :text=>'Enter your username').pack
-entryInUsr = TkEntry.new(signin_win).pack
-labelInPass = TkLabel.new(signin_win, :text=>'Enter your password').pack
-entryInPass = TkEntry.new(signin_win).pack
-TkButton.new(signin_win, :text=>'Submit',
-             :command=>proc{if clients.key?(entryInUsr.get) && clients[entryInUsr.get].validate_pass(entryInPass.get)
-                              signin_win.withdraw
-                              main_win.deiconify
-                              labelInUsr.foreground = 'black'
-                              labelInPass.foreground = 'black'
-                            else
-                              labelInUsr.foreground = 'red'
-                              labelInPass.foreground = 'red'
-                            end}).pack
-
-# signup_win widgets
-labelUpUsr = TkLabel.new(signup_win, :text=>'Enter username').pack
-entryUpUsr = TkEntry.new(signup_win).pack
-labelUpPass = TkLabel.new(signup_win, :text=>'Enter password').pack
-entryUpPass = TkEntry.new(signup_win).pack
-labelUpEml = TkLabel.new(signup_win, :text=>'Enter email').pack
-entryUpEml = TkEntry.new(signup_win).pack
-TkButton.new(signup_win, :text=>'Submit',
-             :command=>proc{if !clients.key?(entryUpUsr.get)
-                              clients[entryUpUsr.get] = Client.new('c2', entryUpUsr.get, entryUpPass.get, entryUpEml.get)
-                              signup_win.withdraw
-                              main_win.deiconify
-                              labelUpUsr.foreground = 'black'
-                              labelUpPass.foreground = 'black'
-                              labelUpEml.foreground = 'black'
-                            else
-                              labelUpUsr.foreground = 'red'
-                            end}).pack
-
-# main_win widgets
-labelWelcome = TkLabel.new(main_win, :text=>'You are signed in').pack
-
-Tk.mainloop
-
-=begin
-root = TkRoot.new
-root.title = "Window"
-
-f1 = TkFrame.new {
-   borderwidth 3
-   background "white"
-   padx 250
-   pady 150
-   pack('side' => 'left')
-}
-
-$resultsVar = TkVariable.new
-StartLbl = TkLabel.new(f1) do
-   textvariable
-   borderwidth 5
-   font TkFont.new('times 20 bold')
-   foreground  "black"
-   pack("side" => "top")
+# Logincont widgets ------------------------------------------------------------
+lblInUsr = Tk::Tile::Label.new(logincont, :text=>'Enter your username').pack
+entInUsr = Tk::Tile::Entry.new(logincont).pack
+lblInPass = Tk::Tile::Label.new(logincont, :text=>'Enter your password').pack
+entInPass = Tk::Tile::Entry.new(logincont).pack
+Tk::Tile::Button.new(logincont, :text=>'Submit',
+    :command=>proc{validate_login(lblInUsr, lblInPass,
+    entInUsr, entInPass, log_win, root)}).pack
+# Signupcont widgets -----------------------------------------------------------
+lblUpUsr = Tk::Tile::Label.new(signupcont, :text=>'Enter username').pack
+entUpUsr = Tk::Tile::Entry.new(signupcont).pack
+Tk::Tile::Label.new(signupcont, :text=>'Enter password').pack
+entUpPass = Tk::Tile::Entry.new(signupcont).pack
+Tk::Tile::Label.new(signupcont, :text=>'Enter email').pack
+entUpEml = Tk::Tile::Entry.new(signupcont).pack
+Tk::Tile::Button.new(signupcont, :text=>'Submit',
+  :command=>proc{validate_signup(lblUpUsr, entUpUsr,
+  entUpPass, entUpEml, log_win, root)}).pack
+# Log/Sign Functions
+def validate_login(lbl1, lbl2, ent1, ent2, curwin, nextwin)
+  if @clients.key?(ent1.get) && @clients[ent1.get].validate_pass(ent2.get)
+    lbl1.foreground = 'black'
+    lbl2.foreground = 'black'
+    curwin.withdraw
+    nextwin.deiconify
+  else
+    lbl1.foreground = 'red'
+    lbl2.foreground = 'red'
+  end
 end
 
-StartLbl['textvariable'] = $resultsVar
-$resultsVar.value = 'Welcome to Fields reservations'
+def validate_signup(lbl1, ent1, ent2, ent3, curwin, nextwin)
+  unless @clients.key?(ent1.get)
+    @clients[ent1.get] = Client.new("c", ent1.get, ent2.get, ent3.get)
+    lbl1.foreground = 'black'
+    curwin.withdraw
+    nextwin.deiconify
+  else
+    lbl1.foreground = 'red'
+  end
+end
+# ==============================================================================
 
-BtnIn = TkButton.new(f1) {
-   text 'Sign In'
-   command {root = TkRoot.new}
-   pack('fill' => 'x')
-}
-BtnUp = TkButton.new(f1) {
-   text 'Sign Up'
-   command {print "pushed sign-up!!\n"}
-   pack('fill' => 'x')
-}
 
 Tk.mainloop
-=end
